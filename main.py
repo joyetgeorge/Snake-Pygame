bcdb34c8576d90e0f01a8947dc54bba74ff630f5
joyetgeorge <joyetgeorge@gmail.com>
2021-12-20 00:27:35 +0530

    reduced

--- main.py
+++ main.py
@@ -5,26 +5,21 @@ import math
 running = True
 pygame.init()
 
-# Snake
+direction = ""
 
 snake_list = []
 temp = []
+score = 0
+
+font = pygame.font.Font('freesansbold.ttf', 15)
 
 snakeX = random.randrange(0, 500, 15)
 snakeY = random.randrange(0, 500, 15)
 
-temp.append(snakeX)
-temp.append(snakeY)
-
-snake_list.append(temp)
-
-# print(snakeX, snakeY)
 snakeX_change = 0
 snakeY_change = 0
-score = 0
 snake_length = 1
 
-# Fruit
 fruitX = random.randrange(0, 500, 15)
 fruitY = random.randrange(0, 500, 15)
 fruitX_change = 0
@@ -32,7 +27,6 @@ fruitY_change = 0
 
 
 screen = pygame.display.set_mode((510, 510))
-
 clock = pygame.time.Clock()
 
 
@@ -52,17 +46,36 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                snakeX_change = 15
-                snakeY_change = 0
+                if direction != "LEFT":
+                    snakeX_change = 15
+                    snakeY_change = 0
+                    direction = "RIGHT"
             if event.key == pygame.K_LEFT:
-                snakeX_change = -15
-                snakeY_change = 0
+                if direction != "RIGHT":
+                    snakeX_change = -15
+                    snakeY_change = 0
+                    direction = "LEFT"
             if event.key == pygame.K_DOWN:
-                snakeY_change = 15
-                snakeX_change = 0
+                if direction != "UP":
+                    snakeY_change = 15
+                    snakeX_change = 0
+                    direction = "DOWN"
             if event.key == pygame.K_UP:
-                snakeY_change = -15
-                snakeX_change = 0
+                if direction != "DOWN":
+                    snakeY_change = -15
+                    snakeX_change = 0
+                    direction = "UP"
+    
+    print(snake_list)
+
+    for i, j in snake_list:
+        if i == fruitX and j == fruitY:
+            fruitX = random.randrange(0, 500, 15)
+            fruitY = random.randrange(0, 500, 15)
+
+    if len(snake_list):
+        if i == snakeX and j == snakeY:
+            print("snake hit")
 
     snakeX += snakeX_change
     snakeY += snakeY_change
@@ -73,21 +86,13 @@ while running:
         fruitX = random.randrange(0, 500, 15)
         fruitY = random.randrange(0, 500, 15)
         score += 1
+
         snake_length += 1
 
     clock.tick(15)
-
     print(score)
     screen.fill([46, 52, 64])
 
-    for i, j in snake_list:
-        if i == fruitX and j == fruitY:
-            fruitX = random.randrange(0, 500, 15)
-            fruitY = random.randrange(0, 500, 15)
-            print("respawn")
-
-        if i == snakeX and j == snakeY:
-            print("snake hit")
 
     temp = []
 
@@ -101,7 +106,9 @@ while running:
 
     for i, j in snake_list:
         pygame.draw.rect(screen, [163, 190, 140], (i, j, 15, 15))
-    print(snake_list)
-
+        if i > 510 or j > 510 or i < 0 or j < 0:
+            running = False
+    text = font.render('SCORE : '+str(score), True, (216, 222, 233))
+    screen.blit(text, (420, 14))
     pygame.draw.rect(screen, [191, 97, 106], (fruitX, fruitY, 15, 15))
     pygame.display.update()
